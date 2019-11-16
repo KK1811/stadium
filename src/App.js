b8ddeaf942ff59f46dafd7e0c5f0212db5f4c8c9
vismith24 <vismith.24.adappa@gmail.com>
2019-11-17 02:55:18 +0530

    Merge Fix

--- src/App.js
+++ src/App.js
@@ -7,14 +7,10 @@ import signup from './components/auth/signup'
 import landingPage from './components/home/landingPage'
 import gamestore from './components/store/gamestore'
 import merchstore from './components/store/merchstore'
-<<<<<<< HEAD
 import friends from './components/user/friends'
 import gamedetails from './components/store/gamedetails'
 import profile from './components/user/profile'
-=======
-import profile from './components/user/profile'
 import loggedin from './components/auth/loggedin'
->>>>>>> 8640fad553a6af75cebaccdb015cd82c37d8f0fb
 // import logout from './components/auth/logout'
 
 function App() {
@@ -26,24 +22,15 @@ function App() {
       {/*<Navbar /> */}
       {/* <br></br><br></br><br></br> */}
       <Switch>
-<<<<<<< HEAD
-        <Route exact path = '/login' component={login} />
-        <Route exact path = '/signup' component={signup} />
-        <Route exact path = '/' component={landingPage} />
-        <Route exact path = '/gamestore' component={gamestore} />
-        <Route exact path = '/merchstore' component={merchstore} />
         <Route exact path = '/friends' component={friends} />
         <Route exact path = '/gamestore/:id' component={gamedetails} />
         <Route exact path = '/profile' component={profile} />
-=======
         <Route exact path='/login' component={login} />
         <Route exact path='/loggedin' component={loggedin} />
         <Route exact path='/signup' component={signup} />
         <Route exact path='/' component={landingPage} />
         <Route exact path ='/gamestore' component={gamestore} />
         <Route exact path ='/merchstore' component={merchstore} />
-        <Route exact path ='/profile' component={profile} />
->>>>>>> 8640fad553a6af75cebaccdb015cd82c37d8f0fb
         {/* <Route exact path='/logout' component={logout} /> */}
       </Switch>
       </BrowserRouter>
